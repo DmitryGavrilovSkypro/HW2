18d25ba5e0a10b1ba1010a6489334125eab82d18
Dmitry <ngmademon@gmail.com>
2024-03-05 23:44:51 +0300

    first commit

---
+++ JS_HM2.js
@@ -0,0 +1,66 @@
+// example1//
+let a = 10
+alert (a)
+a = 20
+alert (a)
+
+// example2//.
+const releaseiPhone = 2007
+alert (releaseiPhone)
+
+// example3//
+const creatorJS = 'Брендан Эйх'
+alert (creatorJS)
+
+// example4//
+let number10 = 10
+let number2 = 2
+let sum = number10 + number2
+let difference = number10 - number2
+let multiplication = number10 * number2
+let division = number10 / number2
+alert (sum)
+alert (difference)
+alert (multiplication)
+alert (division)
+
+// example5//
+let number52 = 2
+let result = (number52 ** 5)
+alert (result)
+
+// example6//
+let number9 = 9
+let number62 = 2
+let remainder = (number9 % number62)
+alert (remainder) 
+
+// example7//
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+// example8//
+let age = prompt ('Сколько вам лет')
+alert(age)
+// example9//
+
+const user = {
+    name : prompt('Dmitry'),
+    age : 32,
+    isAdmin : true,
+}
+user.city = 'city of residence' 
+delete city;
+let info = prompt ('Какую информацию хотите узнать о пользователе?')
+alert (info)
+
+
+// example10//
+let userName = prompt('Введите ваше имя');
+alert(`Привет, ${userName}!`);
